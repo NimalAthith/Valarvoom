227f49cca856fb46d0e7b706af64d69b68e45b44
nimalathith <nimalathith@valarchimsi.localdomain>
2024-02-17 22:56:13 +0530

    Spring Boot Setup

---
+++ valarvoom/src/test/java/com/valarchi/valarvoom/ValarvoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valarchi.valarvoom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValarvoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
