bf0982b7b71c2e3c3ef4c1866785889b2f13fc2e
nimalathith <nimalathith@valarchimsi.localdomain>
2024-02-17 23:10:48 +0530

    Test

--- valarvoom/src/main/java/com/valarchi/valarvoom/ValarvoomApplication.java
+++ valarvoom/src/main/java/com/valarchi/valarvoom/ValarvoomApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+//RestController is important
 @SpringBootApplication
 @RestController
 public class ValarvoomApplication {
