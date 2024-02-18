b83e5c482cfda5ce86557db5411bb5139b11b501
sudharaathith <t.sudharaathith@gmail.com>
2024-02-18 17:04:29 +0530

    summa

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,15 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import Home from './Page/Home'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./Page/Home";
 
 function App() {
-
   return (
-  <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<Home />} /> 
-  </Routes>
-  </BrowserRouter>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
